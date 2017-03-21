4235df4b61e3ee14c08fd28433f01054c1169974
cassshh <casvd@hotmail.com>
2017-03-21 10:33:35 +0100

    Files

---
+++ src/com/battleship/Field.java
@@ -0,0 +1,8 @@
+package com.battleship;
+
+/**
+ * Created by casvd on 20-3-2017.
+ */
+public interface Field {
+    String hit();
+}
---
+++ src/com/battleship/Water.java
@@ -0,0 +1,11 @@
+package com.battleship;
+
+/**
+ * Created by casvd on 20-3-2017.
+ */
+public class Water implements Field {
+    @Override
+    public String hit() {
+        return "Splooshhh";
+    }
+}
